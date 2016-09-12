33feb9ede9a032eaea6c11c19c494aeb2b96f8e5
ouyangshengduo <ouyangshengduo@edan.com.cn>
2016-09-12 11:29:26 +0800

    demo初长成

--- app/src/main/java/oysd/com/android_rx_demo/MainActivity.java
+++ app/src/main/java/oysd/com/android_rx_demo/MainActivity.java
@@ -1,13 +1,58 @@
 package oysd.com.android_rx_demo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import oysd.com.android_rx_demo.utils.DownloadUtil;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TAG = MainActivity.class.getSimpleName();
+
+    private Button button;
+    private ImageView imageView;
+    private DownloadUtil downloadUtil;
+    private String PATH = "http://pic32.nipic.com/20130829/12906030_124355855000_2.png";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        downloadUtil = new DownloadUtil();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                downloadUtil.downloadImage(PATH).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(new Subscriber<byte[]>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.e(TAG,"下载图片操作完成");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG,e.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(byte[] bytes) {
+                        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+                        imageView.setImageBitmap(bitmap);
+                    }
+                });
+
+            }
+        });
     }
 }
